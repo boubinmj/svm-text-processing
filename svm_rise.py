74004e860df29e359a8a984032918ef92dc7c8a4
Matt Boubin <boubinmj@miamioh.edu>
2017-07-20 14:02:32 -0400

    did some unsuccessful experiments

---
+++ svm_rise.py
@@ -0,0 +1,45 @@
+import csv
+import sys, getopt
+
+out = 'AL1.txt'
+boundary_min = 65 
+boundary_max = 85
+
+def write_file(outputfile, inputfile):
+	output_file = open(outputfile, 'a')
+	with open(inputfile) as csvfile:
+    		readCSV = csv.reader(csvfile, delimiter=',')
+    		for row in readCSV:
+			if (int(float(row[0])) >= boundary_min and int(float(row[0])) <= boundary_max):
+				output_file.write('-1 ')
+        			output_file.write('1:')
+				output_file.write(row[0])
+				output_file.write(' 2:')
+				output_file.write(row[1])
+				output_file.write(' 3:')
+				output_file.write(row[3])
+				output_file.write('\n')
+	output_file.close()
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
+   write_file(outputfile, inputfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:]) 
